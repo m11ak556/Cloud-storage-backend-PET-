9dec99858ec3a3449b9b616343d7af9cae5db046
m11ak556 <m11ak89@yandex.ru>
2023-12-01 21:37:43 +0300

    Allowed post requests by disabling csrf

--- src/main/java/com/app/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/com/app/configuration/ApplicationSecurityConfiguration.java
@@ -1,14 +1,19 @@
 package com.app.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 
@@ -40,7 +45,8 @@ public class ApplicationSecurityConfiguration {
         http.
                 authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().permitAll()
-                );
+                )
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
--- src/main/java/com/app/controllers/UserController.java
+++ src/main/java/com/app/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,7 +19,7 @@ public class UserController {
 
     @PostMapping("/user/create")
     @ResponseBody
-    public void createUser(User user) {
+    public void createUser(@RequestParam User user) {
         userRepository.save(user);
     }
 
